f3bc193a1217662b9c90b88df0a7a45df8282f00
Antoine Rousseau <_antoine_@metalu.net>
2020-09-07 19:50:35 +0200

    updates... continue conduite

--- runOSC.sh
+++ runOSC.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-open-stage-control -s localhost:8281 -p 8282 -o 8283 -l teleco.json --url-options hdpi=1
+open-stage-control -s localhost:8281 -p 8282 -o 8283 -l teleco.json 
+#--url-options hdpi=1
