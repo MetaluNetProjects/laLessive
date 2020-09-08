85b3328436e735e2a439c9faf37fecab79a400bc
Antoine Rousseau <_antoine_@metalu.net>
2020-09-08 17:49:54 +0100

    almost finished!

--- runOSC.sh
+++ runOSC.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-open-stage-control -s localhost:8281 -p 8282 -o 8283 -l teleco.json 
+cd ~/laLessive
+node ~/sources/open-stage-control-0.49.12-node -s localhost:8281 -p 8282 -o 8283 -l teleco.json 
 #--url-options hdpi=1
