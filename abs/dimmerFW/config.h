925605ddf710085518596884d430c9f97b19133a
Antoine Rousseau <_antoine_@metalu.net>
2022-03-01 18:00:07 +0100

    use new Fraise dimmers for proj lamps

---
+++ abs/dimmerFW/config.h
@@ -0,0 +1,17 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+// dimmer config for VNH2 port of Versa1
+#define DIMMER_INTPIN K9
+#define DIMMER_K0 MB1
+#define DIMMER_K1 MBEN
+#define DIMMER_K2 MBEN2
+#define DIMMER_K3 MB2
+// dimmer config for VNH1 port of Versa1
+#define DIMMER_K4 MA1
+#define DIMMER_K5 MAEN
+#define DIMMER_K6 MAEN2
+#define DIMMER_K7 MA2
+
+#endif // _CONFIG_H_
+
